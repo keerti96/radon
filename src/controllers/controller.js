500006d6c82e198062791b7fe0f56d60ecfcc91a
maxxhimanshu <hsudhanshu31@gmail.com>
2022-06-30 23:42:15 +0530

    jjkk

--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -2,38 +2,31 @@ const collegeModel = require("../models/collegeModel")
 const internModel = require("../models/internModel")
 const validate = require("../validator/validator.js")
 const createCollege = async function (req, res) {
-    try{
+    try {
         //<<----------Validation of request body parameters--------->>  
         const requestBody = req.body
-        requestBody.name=requestBody.name.toLowerCase().split(" ").join("");
-   
-   
 
-        if (!validate.isValidrequestBody(requestBody)){
+        if (!validate.isValidrequestBody(requestBody)) {
             return res.status(400).send({ status: false, message: "Request body is empty!! Please provide the college details" })
         }
 
-        const {name, fullName, logoLink} = requestBody;
+      
 
-        if(!validate.isValid(name)){
-            return res.status(400).send({ status: false, message: "Please provide the name of college" })
+        const { name, fullName, logoLink } = requestBody;
+
+        let val = validate.checkerCollege(name, fullName, logoLink)
+       
+
+        if (val) {
+            return res.status(400).send({ status: false, message: val })
         }
-
-        if(!validate.isValid(fullName)){
-            return res.status(400).send({ status: false, message: "Please provide the fullName of college" })
-        }
-
-        if(!validate.isValid(logoLink)){
-            return res.status(400).send({ status: false, message: "Please provide the logoLink of college" })
-        }
-
+        requestBody.name = requestBody.name.toLowerCase().split(" ").join("");
         //----check if name is unique
-        const nameCheck=await collegeModel.find({name:name})
-        if(nameCheck.length!=0){
+        const nameCheck = await collegeModel.find({ name: name })
+        if (nameCheck.length != 0) {
             return res.status(400).send({ status: false, message: "this name(abbr) already exists" })
         }
-        if (!/(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-/]))?/.test(requestBody.logoLink))
-        return res.status(400).send({ status: false, message: "Please provide valid link" })
+
 
         //<----create a college document---->
         const savedData = await collegeModel.create(requestBody)
@@ -45,47 +38,51 @@ const createCollege = async function (req, res) {
 
 }
 
-//================================================**Post Api : To create intern data**===========================================================//
-let internData = async  function (req, res){
+//================================================ create intern data**===========================================================//
+let internData = async function (req, res) {
     try {
-        let { name, email, mobile, collegeName } = req.body
-        collegeName=collegeName.toLowerCase();
 
-         //validations starts
+
+        //validations starts
         if (!validate.isValidrequestBody(req.body)) {
-            return res.status(400).send({ status: false, message: "Invalid request parameters. Please provide intern details" })
-            ;
-        }
-       let val= validate.checker(name,email,mobile,collegeName)
-        if(val){
-           return res.status(400).send({ status: false, message: val })
+            return res.status(400).send({ status: false, message: "Invalid request parameters. Please provide intern details" });
         }
 
-       
+        let { name, email, mobile, collegeName } = req.body
+
+        
+
+        let val = validate.checker(name, email, mobile, collegeName)
+        if (val) {
+            return res.status(400).send({ status: false, message: val })
+        }
+
+        collegeName = collegeName.toLowerCase();
+
         const isMobile = await internModel.findOne({ mobile: mobile });
 
         if (isMobile) {
             return res.status(400).send({ status: false, message: "Mobile number already registered" })
-            ;
+                ;
         }
         const emailUsed = await internModel.findOne({ email: email })
-         if (emailUsed) {
-        return  res.status(400).send({ status: false, message:"Email is already registered" })
-        
+        if (emailUsed) {
+            return res.status(400).send({ status: false, message: "Email is already registered" })
+
         }
 
-        
-        const doc = await collegeModel.findOne({name:collegeName})
-        if(!doc){
+
+        const doc = await collegeModel.findOne({ name: collegeName })
+        if (!doc) {
             return res.status(404).send({ status: false, message: "collegeName is not registered" })
-            ;
+                ;
         }
 
         //validation ends
-        let collegeId=doc._id 
-        const result = await internModel.create({name,email, mobile,collegeId})
+        let collegeId = doc._id
+        const result = await internModel.create({ name, email, mobile, collegeId })
         res.status(201).send({ status: true, data: result })
-        
+
 
 
     }
@@ -96,38 +93,35 @@ let internData = async  function (req, res){
 }
 
 
-//=====================================================**Get Api : To get data of interns**======================================================//
+//=====================================================To get data of interns**======================================================//
 
-let collegeDetails = async  (req, res) => {
+let collegeDetails = async function (req, res)  {
     try {
-        let query = req.query.collegeName.toLowerCase()
-
-
-        //validation starts
-        if (!validate.isValid(query)) {
-           return res.status(400).send({ status: false, message: "Invalid request parameters. Please provide intern details" })
-            ;
+        let query = req.query.collegeName
+        
+        let val = validate.checkerIntern(query)
+        if (val) {
+            return res.status(400).send({ status: false, message: val })
         }
-        if (!query.match(/^[a-z,_,-]+$/)) {
-           return res.status(400).send({ status: false, message: "Name should be in valid format" })
-            ;
-        }
+
+      
+        query=query.toLowerCase()
+
         let specificCollege = await collegeModel.findOne({ name: query })
         if (!specificCollege) {
-           return res.status(400).send({ status: true, message: "No college exists with that name" })
-            ;
+            return res.status(400).send({ status: true, message: "No college exists with that name" });
         }
-
         let id = specificCollege._id.toString()
-        let intern = await internModel.find({ collegeId: id, isDeleted: false }).select({_id:1,name:1,
-            email:1,
-            mobile:1})
+        let intern = await internModel.find({ collegeId: id, isDeleted: false }).select({
+            _id: 1, name: 1,
+            email: 1,
+            mobile: 1
+        })
 
-        if(!validate.isValidrequestBody(intern)){
-            return res.status(400).send({status:false, message: "no intern is regestered"});
+        if (!validate.isValidrequestBody(intern)) {
+            return res.status(400).send({ status: false, message: "no intern is regestered" });
         }
-        
-        //validations ends 
+
         let data = {                                             //new object 
             name: specificCollege.name,
             fullName: specificCollege.fullName,
@@ -144,6 +138,6 @@ let collegeDetails = async  (req, res) => {
 
 
 
-module.exports = { createCollege,internData,collegeDetails }
+module.exports = { createCollege, internData, collegeDetails }
 
 
--- src/validator/validator.js
+++ src/validator/validator.js
@@ -3,7 +3,7 @@
 const mongoose = require("mongoose");
 
 const isValid = function (value) {
-    if (typeof value === 'undefined' || value === 'null') return false     
+    if (typeof value === 'undefined' || value === 'null') return false
     if (typeof value === 'string' && value.trim().length === 0) return false
     return true
 }
@@ -13,47 +13,91 @@ const isValidrequestBody = function (requestBody) {
 
 
 
-const checker=function (name,email,mobile,collegeName){
-    let missdata="";
+const checker = function (name, email, mobile, collegeName) {
+    let missdata = "";
 
     if (!isValid(name)) {
-         missdata=missdata+"name"
-      
+        missdata = missdata + "name"
+
     }
-   if (!isValid(email)) {
-        missdata=missdata+", "+"email"
-       
+    if (!isValid(email)) {
+        missdata = missdata + ", " + "email"
+
     }
-   if (!isValid(mobile)) {
-        missdata=missdata+" , "+"mobile"
-       
+    if (!isValid(mobile)) {
+        missdata = missdata + " , " + "mobile"
+
     }
-    if(!collegeName){
-        missdata=missdata+" , "+"collegeName"
-        
+    if (!collegeName) {
+        missdata = missdata + " , " + "collegeName"
+
     }
-    if(missdata){
-        return missdata+" is missing"
+    if (missdata) {
+        return missdata + " is missing"
     }
     // if (!/^[a-z,',-]+(\s)[a-z,',-]+$/i.test(name)) {
     //     missdata=missdata+"Name is invalid " 
-       
+
     // }
 
-  
+
     if (!/^[a-z0-9]{1,}@g(oogle)?mail\.com$/.test(email)) {
-        missdata=missdata+ "Email is invalid" 
-      
+        missdata = missdata + "Email is invalid"
+
     }
 
-    
+
 
     if (!/^(?:(?:\+|0{0,2})91(\s*[\-]\s*)?|[0]?)?[6789]\d{9}$/.test(mobile)) {
-        missdata=missdata+ "Mobile number should be in valid format" 
-        
+        missdata = missdata + "Mobile number should be in valid format"
+
     }
 
     return missdata;
 
 }
-module.exports = {checker ,isValid ,isValidrequestBody}
+const checkerCollege = function (name, fullName, logoLink) {
+    
+    let missdata = "";
+    if (!isValid(name)) {
+        missdata = missdata + " name of college" 
+    }
+
+    if (!isValid(fullName)) {
+        missdata = missdata +  "  fullName of college" 
+    }
+
+    if (!isValid(logoLink)) {
+         missdata = missdata + " logoLink of college" 
+    }
+    if(missdata){return missdata +"  is missing"};
+    //  if (/\d/.test(name)) {
+    //     missdata=missdata+ " name cannot have numbers.   "
+       
+    //  }
+    if (!/(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-/]))?/.test(logoLink)){
+        return missdata = missdata +  " Please provide valid link" 
+    }
+
+    if (!name.match(/^[a-z,_,-]+$/)) {
+        return  "Name should be in valid format"
+
+    }
+}
+
+const checkerIntern = function (query) {
+            //validation starts
+            if (!validate.isValid(query)) {
+                return "Invalid request parameters. Please provide intern details" ;
+            }
+            if (!query.match(/^[a-z,_,-]+$/)) {
+                return "Name should be in valid format" ;
+            }
+           
+}
+
+
+
+
+module.exports = { checker, isValid, isValidrequestBody, checkerCollege,checkerIntern }
+
