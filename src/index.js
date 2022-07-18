1b8293b10669166741e3cd2e792c29bfac3945a3
jagcho <jagadishchodry@gmail.com>
2022-07-18 13:40:50 +0530

    a

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const express = require("express")
+const bodyParser = express.json()
+const mongoose = require("mongoose")
+const app = express()
+const route = require("./routes/route")
+
+app.use(bodyParser)
+
+mongoose.connect("mongodb+srv://Jagcho:71nEXJtXcYfVx8T6@cluster0.5bg4mzz.mongodb.net/group25Database"
+    , { useNewUrlParser: true })
+    .then(() => console.log("MongoDb is connected"))
+    .catch(err => console.log(err))
+
+app.use("/", route)
+
+
+
+app.listen(process.env.PORT || 3000, function () { console.log("Express is running on port " + (process.env.PORT || 3000)) });
